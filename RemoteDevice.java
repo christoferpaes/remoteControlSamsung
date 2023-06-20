facc270dec73c7633bee9acd87cedb79a23ed842
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-06-19 18:34:03 -0700

    Create RemoteDevice.java

---
+++ RemoteDevice.java
@@ -0,0 +1,14 @@
+package com.samsung.android.sdk.remoteservice;
+
+public class RemoteDevice {
+
+    private String ip;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
